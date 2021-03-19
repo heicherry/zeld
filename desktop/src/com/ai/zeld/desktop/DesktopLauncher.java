fa7c9f5377dc48840d6bbe730ceebde5b32a740f
Jimmy Ai <2625171028@qq.com>
2021-03-19 23:36:55 +0800

    init repository

---
+++ desktop/src/com/ai/zeld/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ai.zeld.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ai.zeld.Zeld;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Zeld(), config);
+	}
+}
